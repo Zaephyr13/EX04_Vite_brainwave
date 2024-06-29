a9a5916773385eaccb66355d7a7743bb1952ee25
Christian ANTUNA <chriscorps@hotmail.com>
2024-06-29 22:10:32 +0200

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+/** @format */
+
+export default {
+    plugins: {
+        'postcss-import': {},
+        'tailwindcss/nesting': 'postcss-nesting',
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}
