c125bc33979f5d0b7f3a752957149f092b3b0b04
Christian ANTUNA <chriscorps@hotmail.com>
2024-07-23 14:17:19 +0200

    Forgot to commit.. x) Add Benefits section ; create Heading & Section & Tagline reusable components ; add services, roadmap, hero, collaboration & pricing sections.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 /** @format */
 import ButtonGradient from "./assets/svg/ButtonGradient"
 import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Benefits from "./components/Benefits"
+import Collaboration from "./components/Collaboration"
+import Services from "./components/Services"
+import Pricing from "./components/Pricing"
+import Roadmap from "./components/Roadmap"
 
 const App = () => {
-    return (
-        <>
-            <div className="pt-[4.75rem] ld:pt-[5.25rem] overflow-hidden">
-                <Header />
-            </div>
-            <ButtonGradient />
-        </>
-    )
+  return (
+    <>
+      <div className="pt-[4.75rem] ld:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+      </div>
+      <ButtonGradient />
+    </>
+  )
 }
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 /** @format */
+
 import { useLocation } from "react-router-dom"
-import { useState } from "react"
 import { disablePageScroll, enablePageScroll } from "scroll-lock"
 
 import { brainwave } from "../assets"
 import { navigation } from "../constants"
-import { HamburgerMenu } from "./design/Header"
-
 import Button from "./Button"
 import MenuSvg from "../assets/svg/MenuSvg"
+import { HamburgerMenu } from "./design/Header"
+import { useState } from "react"
 
 const Header = () => {
   const pathname = useLocation()
@@ -33,7 +33,7 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
@@ -41,6 +41,7 @@ const Header = () => {
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
+
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
@@ -54,20 +55,20 @@ const Header = () => {
                 onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold 
-                ${
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
                   item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
-                } 
-                lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
                 {item.title}
               </a>
             ))}
           </div>
+
           <HamburgerMenu />
         </nav>
+
         <a
           href="#signup"
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
@@ -79,7 +80,7 @@ const Header = () => {
         </Button>
 
         <Button
-          className="ml-auto ld:hidden"
+          className="ml-auto lg:hidden"
           px="px-3"
           onClick={toggleNavigation}
         >
